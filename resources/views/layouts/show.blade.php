32a8d10483ce9b8aa47ce4d37f98830f51713c64
shijiaqi <2280191633@qq.com>
2019-12-21 16:09:51 +0800

    21号

--- resources/views/layouts/show.blade.php
+++ resources/views/layouts/show.blade.php
@@ -2,33 +2,27 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
+	<link rel="icon" type="image/png" href="/assets/img/favicon.ico">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<title>Home</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
-
-
     <!-- Bootstrap core CSS     -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
     <!-- Animation library for notifications   -->
-    <link href="assets/css/animate.min.css" rel="stylesheet"/>
-
+    <link href="/assets/css/animate.min.css" rel="stylesheet"/>
     <!--  Light Bootstrap Table core CSS    -->
-    <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
-
-
+    <link href="/assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
     <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="assets/css/demo.css" rel="stylesheet" />
+    <link href="/assets/css/demo.css" rel="stylesheet" />
 
 
     <!--     Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <link href="/assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
 
 </head>
 <body>
@@ -74,7 +68,7 @@
                         <i class="pe-7s-map-marker"></i>
                         <p id=''>汽车展示</p>
                     </a>
-                    <a href="#" class='none' sta='1'>
+                    <a href="{{url('cart/create')}}" class='none' sta='1'>
                         <i class="pe-7s-map-marker"></i>
                         <p id=''>汽车添加</p>
                     </a>
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,17 @@
 // Route::get('/layouts', function () {
 //     return view('layout');
 // });
+
 /**后台 */
-Route::get('/','admin\Admin@index');
+Route::get('/','admin\admin@index');
+
+//商品商品模板
+Route::prefix('cart')->group(function () {
+    Route::get('/','admin\Cart@index');//展示页面
+    Route::get('create','admin\Cart@create');//添加视图
+    Route::post('store','admin\Cart@store');//执行添加
+    Route::get('destroy/{id}','admin\Cart@destroy');//执行删除
+    Route::get('edit/{id}','admin\Cart@edit');//编辑视图
+    Route::post('update/{id}','admin\Cart@update');//执行修改
+    Route::post('checkonly','admin\Cart@checkonly');//ajax唯一
+});
